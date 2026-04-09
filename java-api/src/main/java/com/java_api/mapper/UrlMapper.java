527b32f358c276b19f308a5b0eb284fa16063801
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-09 18:42:17 -0300

    feat: add url mapper

---
+++ java-api/src/main/java/com/java_api/mapper/UrlMapper.java
@@ -0,0 +1,22 @@
+package com.java_api.mapper;
+
+import com.java_api.dto.UrlDTO;
+import com.java_api.model.Url;
+
+public class UrlMapper {
+    public static UrlDTO toDTO(Url url) {
+        return new UrlDTO(
+                url.getId(),
+                url.getUrl(),
+                url.getShortUrl()
+        );
+    }
+
+    public static Url toModel(UrlDTO urlDTO) {
+        return new Url(
+                urlDTO.id(),
+                urlDTO.url(),
+                urlDTO.shortUrl()
+        );
+    }
+}
