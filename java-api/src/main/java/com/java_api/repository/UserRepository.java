7fd6c6f5a55e84d4c7fd9d9be2f8882dfbc92255
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-10 08:15:52 -0300

    feat: add user repository

---
+++ java-api/src/main/java/com/java_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.java_api.repository;
+
+import com.java_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
