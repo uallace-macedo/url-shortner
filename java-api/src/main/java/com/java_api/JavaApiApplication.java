2df246d2151d0e4f8a3b2f3a175d551727909bb3
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-10 08:13:54 -0300

    feat: enableJpaAuditing

--- java-api/src/main/java/com/java_api/JavaApiApplication.java
+++ java-api/src/main/java/com/java_api/JavaApiApplication.java
@@ -2,8 +2,10 @@ package com.java_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JavaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApiApplication.class, args);
