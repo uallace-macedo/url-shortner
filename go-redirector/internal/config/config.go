5488d01ff888b6a7ea05b15ea9129eca3517d502
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-10 11:01:01 -0300

    feat: add PSQL envs

--- go-redirector/internal/config/config.go
+++ go-redirector/internal/config/config.go
@@ -8,12 +8,23 @@ import (
 
 type Config struct {
 	APIPort string
+
+	PSQLHost     string
+	PSQLUser     string
+	PSQLPassword string
+	PSQLDatabase string
+	PSQLPort     string
 }
 
 func Load() *Config {
 	_ = godotenv.Load("../.env")
 	return &Config{
-		APIPort: getEnv("GO_REDIRECTOR_PORT", ":4001"),
+		APIPort:      getEnv("GO_REDIRECTOR_PORT", ":4001"),
+		PSQLHost:     getEnv("PSQL_HOST", ""),
+		PSQLUser:     getEnv("PSQL_USER", ""),
+		PSQLPassword: getEnv("PSQL_PASSWORD", ""),
+		PSQLDatabase: getEnv("PSQL_DATABASE", ""),
+		PSQLPort:     getEnv("PSQL_EXTERNAL_PORT", ""),
 	}
 }
 
