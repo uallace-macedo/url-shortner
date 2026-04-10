3515e34d978c4595fbb6d491a3e11ff726b875d9
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-10 08:43:02 -0300

    feat: add logger

---
+++ go-redirector/internal/config/logger.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	writer  io.Writer
+}
+
+const (
+	reset  = "\033m[0m"
+	red    = "\033m[31m"
+	green  = "\033m[32m"
+	yellow = "\033m[33m"
+	blue   = "\033m[34m"
+)
+
+func NewLogger(p string) *Logger {
+	writer := io.Writer(os.Stdout)
+	logger := log.New(writer, p, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug:   log.New(logger.Writer(), blue+"[DEBUG] "+reset, logger.Flags()),
+		info:    log.New(logger.Writer(), green+"[INFO] "+reset, logger.Flags()),
+		warning: log.New(logger.Writer(), yellow+"[WARNING] "+reset, logger.Flags()),
+		err:     log.New(logger.Writer(), red+"[ERROR] "+reset, logger.Flags()),
+		writer:  logger.Writer(),
+	}
+}
+
+func (l *Logger) Debug(v ...any) {
+	l.debug.Println(v...)
+}
+
+func (l *Logger) Info(v ...any) {
+	l.info.Println(v...)
+}
+
+func (l *Logger) Warning(v ...any) {
+	l.warning.Println(v...)
+}
+
+func (l *Logger) Error(v ...any) {
+	l.err.Println(v...)
+}
+
+func (l *Logger) Debugf(fmts string, v ...any) {
+	l.debug.Printf(fmts, v...)
+}
+
+func (l *Logger) Infof(fmts string, v ...any) {
+	l.info.Printf(fmts, v...)
+}
+
+func (l *Logger) Warningf(fmts string, v ...any) {
+	l.warning.Printf(fmts, v...)
+}
+
+func (l *Logger) Errorf(fmts string, v ...any) {
+	l.err.Printf(fmts, v...)
+}
