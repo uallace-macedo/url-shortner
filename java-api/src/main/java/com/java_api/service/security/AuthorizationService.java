8f64471e7ccb109914efb5c9462c5804baa02946
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-11 11:09:13 -0300

    feat: add authorizationservice (userDetailsService)

---
+++ java-api/src/main/java/com/java_api/service/security/AuthorizationService.java
@@ -0,0 +1,20 @@
+package com.java_api.service.security;
+
+import com.java_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Wrong credentials"));
+    }
+}
