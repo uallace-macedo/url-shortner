68fdf9353fcb9d1a9b4bc0644b7f362bed3bd9f2
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-15 18:46:12 -0300

    style: change dto name

--- java-api/src/main/java/com/java_api/controller/dto/user/UserRegisterRequest.java
+++ java-api/src/main/java/com/java_api/controller/dto/user/UserRegisterRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
-public record UserCreateRequest(
+public record UserRegisterRequest(
         @NotBlank(message = "Please provide a name")
         @Length(min = 4, message = "Name must be at least 4 characters")
         String name,
