d2950e4a4c01a4cfc215e304e433ca887424598d
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-16 08:11:21 -0300

    feat: add create url

--- java-api/src/main/java/com/java_api/controller/dto/url/UrlRequestDTO.java
+++ java-api/src/main/java/com/java_api/controller/dto/url/UrlRequestDTO.java
@@ -1,8 +1,23 @@
 package com.java_api.controller.dto.url;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.OffsetDateTime;
 
 public record UrlRequestDTO(
         @NotBlank(message = "URL cannot be void")
-        String url
+        String url,
+
+        @Size(min = 5, max = 30, message = "Custom slug must be between 5 & 30 characters")
+        @JsonProperty("custom_slug")
+        String customSlug,
+
+        @JsonProperty("expires_at")
+        OffsetDateTime expiresAt,
+
+        @JsonProperty("max_click_count")
+        Long maxClickCount
 ) {}
