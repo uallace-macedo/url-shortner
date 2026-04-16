207223edd6292063f9dbccb244346dd129e77726
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-16 08:10:44 -0300

    feat: add custom functions

--- java-api/src/main/java/com/java_api/repository/UrlRepository.java
+++ java-api/src/main/java/com/java_api/repository/UrlRepository.java
@@ -4,7 +4,9 @@ import com.java_api.model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
-    Optional<Url> findByUrl(String url);
+    Optional<Url> findByUrlAndUserId(String url, UUID userId);
+    Optional<Url> findByCustomSlug(String customSlug);
 }
